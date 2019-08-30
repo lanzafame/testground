ce63e3c3e6455657b7c6b3c5b1221d12100d4330
Jim Pick <jim@backblend.com>
2019-08-29 22:07:14 -0700

    Don't run add-get tests, seem to fail on Cloud Run

--- plans/smlbench/cases/inventory.go
+++ plans/smlbench/cases/inventory.go
@@ -14,12 +14,10 @@ var TestCases = []smlbench.SmallBenchmarksTestCase{
 	&simpleAddTC{2 * 1024 * 1024},  // 2mb
 	&simpleAddTC{5 * 1024 * 1024},  // 5mb
 	&simpleAddTC{10 * 1024 * 1024}, // 10mb
-	*/
 
 	&simpleAddGetTC{},                 // 0
 	&simpleAddGetTC{1024},             // 1kb
 	&simpleAddGetTC{64 * 1024},        // 64kb
-	/*
 	&simpleAddGetTC{256 * 1024},       // 256kb
 	&simpleAddGetTC{512 * 1024},       // 512kb
 	&simpleAddGetTC{1024 * 1024},      // 1mb
