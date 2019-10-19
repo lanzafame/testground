90a18f18bfc8b15f304302c3692986c27da7c9ab
Jim Pick <jim@protocol.ai>
2019-10-19 22:03:51 +0000

    Add random file on adder peer

--- plans/smlbench2/go.mod
+++ plans/smlbench2/go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/dustin/go-humanize v1.0.0
+	github.com/ipfs/go-ipfs-api v0.0.2
 	github.com/ipfs/testground/sdk/iptb v0.0.0-00010101000000-000000000000
 	github.com/ipfs/testground/sdk/runtime v0.0.0-00010101000000-000000000000
 	github.com/ipfs/testground/sdk/sync v0.0.0-20191017072543-376444a0dd33
--- plans/smlbench2/main.go
+++ plans/smlbench2/main.go
@@ -3,46 +3,29 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"time"
 
-	// test "github.com/ipfs/testground/plans/smlbench2/test"
-	// utils "github.com/ipfs/testground/plans/smlbench2/utils"
+	utils "github.com/ipfs/testground/plans/smlbench2/utils"
 	iptb "github.com/ipfs/testground/sdk/iptb"
 	"github.com/ipfs/testground/sdk/runtime"
 	"github.com/ipfs/testground/sdk/sync"
 	"github.com/libp2p/go-libp2p-core/peer"
+	shell "github.com/ipfs/go-ipfs-api"
 )
 
-// Inventory of Tests
-/*
-var testCasesSet = [][]utils.SmallBenchmarksTestCase{
-	{
-		&test.SimpleAddGetTC{10 * 1024 * 1024}, // 10mb
-	},
-}
-*/
+const (
+	sizeBytes = 10 * 1024 * 1024 // 10mb
+)
 
-// TODO:
-//  Testcase abstraction.
-//  Entrypoint demuxing (TEST_CASE_SEQ).
-//  Pipe stdout to intercept messages.
-//  Temporary directory from environment variable.
-//  Error handling -- right now everything panics on failure.
 func main() {
-	// _ = os.Setenv("TEST_PLAN", "smlbenchmarks")
-	// _ = os.Setenv("TEST_BRANCH", "master")
-	// _ = os.Setenv("TEST_TAG", "")
-	// _ = os.Setenv("TEST_RUN", uuid.New().String())
-
 	runenv := runtime.CurrentRunEnv()
 	if runenv.TestCaseSeq < 0 {
 		panic("test case sequence number not set")
 	}
 
-	// testCases := testCasesSet[runenv.TestCaseSeq]
-
 	watcher, writer := sync.MustWatcherWriter(runenv)
         defer watcher.Close()
-	fmt.Printf("Jim1 writer %v\n", writer)
 
 	spec := iptb.NewTestEnsembleSpec()
 	spec.AddNodesDefaultConfig(iptb.NodeOpts{Initialize: true, Start: true}, "local")
@@ -53,51 +36,60 @@ func main() {
 
 	localNode := ensemble.GetNode("local")
 
-	fmt.Println("Jim localNode", localNode)
-	fmt.Println("Jim", *localNode)
-	fmt.Printf("Jim2 %T\n", *localNode)
-	// fmt.Printf("Jim3 %v\n", *localNode.PeerID)
 	peerID, err := localNode.PeerID()
 	if err != nil {
-		panic(err)
+		runenv.Abort(err)
 	}
 	fmt.Println("Jim peerID", peerID)
 
 	ID, err := peer.IDB58Decode(peerID)
 	if err != nil {
-		panic(err)
+		runenv.Abort(err)
 	}
 	addrInfo := &peer.AddrInfo{
 		ID: ID,
 	}
-	fmt.Println("Jim addrInfo", addrInfo)
 
 	seq, err := writer.Write(sync.PeerSubtree, addrInfo)
 	if err != nil {
-                panic(err)
+                runenv.Abort(err)
         }
-	fmt.Println("Jim seq", seq)
-	/*
-	if _, err = writer.Write(sync.PeerSubtree, host.InfoFromHost(h)); err != nil {
-                panic(err)
-        }
-	*/
         defer writer.Close()
 
 	client := localNode.Client()
-	fmt.Printf("Jim client %v\n", client)
-	/*
-	*/
 
-	fmt.Println("Jim Run test here")
-
-	/*
-	for _, tc := range testCases {
-
-		tc.Execute(runenv, ensemble)
-
-		ensemble.Destroy()
+	switch {
+	case seq == 1:
+		runAdder(runenv, ensemble, client, sizeBytes)
+	case seq == 2:
+		// runGetter(client)
+	default:
+		runenv.Abort(fmt.Errorf("Unexpected seq: %v", seq))
 	}
-	*/
+
 	ensemble.Destroy()
 }
+
+func runAdder(runenv *runtime.RunEnv, ensemble *iptb.TestEnsemble, adder *shell.Shell, size int64) {
+	fmt.Println("adder")
+
+	// generate a random file of the designated size.
+        file := utils.TempRandFile(runenv, ensemble.TempDir(), size)
+        defer os.Remove(file.Name())
+
+        tstarted := time.Now()
+        cid, err := adder.Add(file)
+        if err != nil {
+                runenv.Abort(err)
+                return
+        }
+	fmt.Println("cid", cid)
+
+        runenv.EmitMetric(utils.MetricTimeToAdd, float64(time.Now().Sub(tstarted)/time.Millisecond))
+}
+
+/*
+func runGetter(getter *shell.Shell) {
+	fmt.Println("getter")
+}
+*/
